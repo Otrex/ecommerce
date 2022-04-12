6d656b2a1c8e94d00958b372f2c8eefc8b8b32e3
Treasure Obisike <treasure.obisike@chigisoft.com>
2022-04-12 11:01:13 +0100

    fixed auth.service 'type' issue

--- src/server.js
+++ src/server.js
@@ -7,12 +7,14 @@ dotenv.config(path.join(__dirname, '../.env'));
 const app = require('./app');
 
 const config = require('./config');
-const { generalLogger } = require('./core/Logger');
+const Logger = require('./core/Logger');
+
+const log = new Logger();
 
 require('./database')
   .connect()
   .then(() => {
     app.listen(config.app.port, () => {
-      generalLogger.info(`SERVER STARTED ::: PORT=${config.app.port}`);
+      log.info(`SERVER STARTED ::: PORT=${config.app.port}`);
     });
   });
